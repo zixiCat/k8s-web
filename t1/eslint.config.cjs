7a98a0bd6ddb65cd58cf4c9e0d1bc0bcfad6cb8a
zixiCat <zixicat@outlook.com>
2024-12-27 15:48:03 +0800

    feat: init nx

---
+++ t1/eslint.config.cjs
@@ -0,0 +1,28 @@
+const nx = require('@nx/eslint-plugin');
+
+module.exports = [
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  {
+    ignores: ['**/dist'],
+  },
+  {
+    files: [
+      '**/*.ts',
+      '**/*.tsx',
+      '**/*.js',
+      '**/*.jsx',
+      '**/*.cjs',
+      '**/*.mjs',
+    ],
+    // Override or add rules here
+    rules: {},
+  },
+  ...nx.configs['flat/react'],
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {},
+  },
+];
---
+++ t1/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="t1" />
+    </div>
+  );
+}
+
+export default App;
